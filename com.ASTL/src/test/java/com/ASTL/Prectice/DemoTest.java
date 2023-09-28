ef0417a9527160c85be97bd9a94285372a9b0cd9
User <User@DESKTOP-H0OUOOC>
2023-09-28 09:28:27 +0530

    Added package for Practice

---
+++ com.ASTL/src/test/java/com/ASTL/Prectice/DemoTest.java
@@ -0,0 +1,12 @@
+package com.ASTL.Prectice;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	@Test
+	public void displayMessage()
+	{
+		System.out.println("--Demo Message--");
+	}
+
+}
